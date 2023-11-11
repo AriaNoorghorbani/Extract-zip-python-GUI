6d2c0e51a9b5f91eaf050be5c9c7c0852e45b6f3
arianoorqorbani <aria.noorghorbani@gmail.com>
2023-11-11 11:56:04 +0300

    Complete the app

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import PySimpleGUI as sg
+from extractor import extractor
 
 file_label = sg.Text('Choose File:    ')
 file_input = sg.Input()
-file_button = sg.Button(' Select File  ')
+file_button = sg.FileBrowse(' Select File  ')
 
 folder_label = sg.Text('Choose Folder:')
 folder_input = sg.Input()
-folder_button = sg.Button('Select Folder')
+folder_button = sg.FolderBrowse('Select Folder')
 
 extract_button = sg.Button('Extract')
-success_message = sg.Text()
+success_message = sg.Text(key='success')
 
 window = sg.Window('Extract File', [
     [file_label, file_input, file_button],
@@ -17,5 +18,18 @@ window = sg.Window('Extract File', [
     [extract_button, success_message]
 ])
 
-window.read()
+while True:
+    event, value = window.read()
+    print(event)
+    print(value)
+    match event:
+        case "Extract":
+            file_path = value[' Select File  ']
+            folder = value['Select Folder']
+            print(file_path)
+            print(folder)
+            extractor(file_path, folder)
+            window['success'].update(value="Extracted Successfully")
+        case sg.WINDOW_CLOSED:
+            break
 window.close()
